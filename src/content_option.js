893f59027ba930917b8e6e61b6cfd90bc58de385
Vihaan <vihaanchugh2811@gmail.com>
2023-07-07 16:15:45 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "VC";
+const meta = {
+    title: "Vihaan Chugh",
+    description: "I’m Vihaan Chugh, a BSc CS Specialist student at Univesity of Toronto",
+};
+
+const introdata = {
+    title: "I’m Vihaan Chugh",
+    animated: {
+        first: "I love programming",
+        second: "I like making fun websites",
+        third: "I want to create a difference",
+    },
+    description: "Hey everyone, I am just a passionate computer science enthusiast, who is driven by an insatiable curiosity for all things related to technology and its applications.",
+   your_img_url : "https://i.pinimg.com/originals/5d/74/0d/5d740d011dab6ba9243dcc3670f0bf82.jpg" ,
+};
+
+const dataabout = {
+    title: "My goals and Aspirations ",
+    aboutme: "My deep-rooted interest in computer science has led me to continuously explore various areas within the field. Whether it's diving into complex algorithms, tackling challenging programming problems, or researching emerging technologies, I am always eager to expand my knowledge and skill set. Driven by my passion for computer science, I am eager to embark on a career path that allows me to make a positive impact through technology. I am excited to contribute my knowledge, skills, and enthusiasm to projects that push boundaries, innovate, and shape the future of the digital world.",
+};
+const worktimeline = [{
+        jobtitle: "Software Engineer Intern",
+        where: "R systems",
+        date: "2023",
+        experience: "Created a Candidate Evaluation System for the Company using - HTML, CSS, JavaScript, Bootstrap, Python and MySQL "
+        
+    },
+    {
+        jobtitle: "FrontEnd Development Engineer",
+        where: "Mindsprint",
+        date: "2022",
+        experience: "Frontend Development Intern, designing and developing a project using HTML, CSS, React Native, and JavaScript, resulting in a fully working and visually appealing application."
+    },
+    
+];
+
+const skills = [{
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "Djano",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "ReactJS",
+        value: 70,
+    },
+    {
+        name: "Reactnative",
+        value: 70,
+    },
+    {
+        name: "HTML & CSS",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "University of Toronto 2022-2026",
+        description: "Pursuing a Bachelor of Science degree, specialising in computer science. ",
+    },
+    {
+        title: "American International School of Lagos",
+        description: "I completed the IB diploma from AISL in 2021.",
+    },
+
+];
+
+const dataportfolio = [{
+        img: "https://www.eternity.eco/wp-content/uploads/2022/09/ProjectEternityLogo-NewSabrina.png",
+        description: "Intergenerational Applied Spiritual Wisdom In The Age of AI",
+        link: "https://www.eternity.eco/",
+    },
+    {
+        img: "https://nationaltoday.com/wp-content/uploads/2021/12/Play-Basketball-Day.jpg",
+        description: "Basketball and Bettering (Basketbet",
+        link: "basketbet.org",
+    },
+    {
+        img: "https://uoftflutterdevs.org/images/No%20Background.png",
+        description: "Using Flutter to create mobile apps (website contribution).",
+        link: "https://uoftflutterdevs.org/",
+    },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vihaanchugh2811@gmail.com",
+    YOUR_FONE: "+1(647)5697889",
+    description: "Feel Free to contact me regarding anything! ",
+    YOUR_SERVICE_ID: "service_8lw6gpc",
+    YOUR_TEMPLATE_ID: "template_vmqa0r7",
+    YOUR_USER_ID: "BbYihIeObjLhVjLYa",
+};
+
+const socialprofils = {
+    github: "https://github.com/vivic04",
+    linkedin: "https://www.linkedin.com/in/vihaan-chugh-349a4023b/",
+    twitter: "https://twitter.com/chugh_vihaan",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/resume/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import {meta} from "../../content_option";
+
+
+
+export const Resume = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Resume | {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Resume</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="ResumeEmbed">
+            <body>
+                <iframe id = "MyPdf" src = "Resume 2023.pdf" title="Resume Embed"  height={750} width={800}> </iframe>
+            </body>
+            
+        </Row>
+        <Row>
+
+        </Row>
+      </Container>
+    </HelmetProvider>
+    
+  )};
+
